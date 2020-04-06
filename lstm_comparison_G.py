2ae38e39e708c8b304caf1ac5fd5fbff4aa1461e
Herman Wika Horn <herman.wika.horn@entur.org>
2020-04-06 22:09:53 +0200

    Finalized regularization example

--- lstm_comparison_G.py
+++ lstm_comparison_G.py
@@ -2,47 +2,7 @@ import src.core as mlApi
 
 # 1. Define dataset specifics
 
-# File path to dataset .csv file
-filename = "../master-thesis-db/datasets/G/data_10min.csv"
-
-# List of columns on form ['name', 'desc', 'unit']
-columns = [
-	['PDI0064', 'Process side dP', 'bar'],
-	['TI0066', 'Process side Temperature out','degrees'],
-	['TZI0012', 'Process side Temperature in', 'degrees'],
-	['FI0010', 'Process side flow rate', 'MSm^3/d(?)'],
-	['TT0025', 'Cooling side Temperature in', 'degrees'],
-	['TT0026', 'Cooling side Temperature out', 'degrees'],
-	['PI0001', 'Cooling side Pressure in', 'barG'],
-	['FI0027', 'Cooling side flow rate', 'MSm^3/d(?)'],
-	['TIC0022U', 'Cooling side valve opening', '%'],
-	['PDT0024', 'Cooling side dP', 'bar'],
-]
-
-# List of column names to ignore completely
-irrelevantColumns = [
-	'PI0001',
-	'FI0027',
-	'TIC0022U',
-	'PDT0024',
-]
-
-# List of column names used a targets
-targetColumns = [
-	'TT0026',
-    'PDI0064',
-]
-
-# List of training periods on form ['start', 'end']
-traintime = [
-	["2019-04-10 00:00:00", "2019-08-01 00:00:00"]
-]
-
-# Testing period, recommended: entire dataset
-testtime = [
-	"2017-01-01 00:00:00",
-	"2020-03-01 00:00:00",
-]
+filename, columns, irrelevantColumns, targetColumns, traintime, testtime, columnOrder = configs.getConfig('G', 'C', '10min')
 
 # 2. Initiate and divide data
 
